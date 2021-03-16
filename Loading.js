4f9363d5ff20fbe5bb9037eea489f7c0e4fd196f
김혜민 <alsdk3586@hanmail.net>
2021-03-16 13:50:46 +0900

    make a loading page

---
+++ Loading.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons"
+import {StyleSheet, Text,View} from "react-native";
+
+export default function Loading(){
+    return <View style={styles.container}>
+        <View style={styles.loading}>
+            <Icon name="weather-cloudy" size={200}  color="#fff"></Icon>
+            <Text style={styles.loadingText}>The WeatherApp</Text>
+        </View>
+    </View>;
+}
+const styles= StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  loading:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:"#9fcdfb"
+  },
+  loadingText:{
+    fontSize:30
+  }
+});
